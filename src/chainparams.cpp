141043cb4f5c58a5e298e03b87b0f8ab87f78206
TheRealOxyd69 <43060236+TheRealOxyd69@users.noreply.github.com>
2018-09-28 12:22:21 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -3,7 +3,6 @@
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The PIVX developers
 // Copyright (c) 2017-2018 The Bitcoin Green developers
-// Copyright (c) 2018 The Rekel developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -54,14 +53,18 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-    (0, uint256("0x09229261a9715b25eb4e1f63f35825df173b1cf1f41d1cf3dcf6309fd196614b"));
+    (0,      uint256("0x000008467c3a9c587533dea06ad9380cded3ed32f9742a6c0c1aebc21bf2bc9b"))
+    (263046, uint256("0x3eba7f1e1b4a076d5333681bb9b8ee2aeac9eead6c82c8e65405726dda355826"))
+    (278818, uint256("0x86333b5dbcf898cfbc33038a10e935f2ecfd88aa8a967e2bc39c6dd9cc6c2e93"))
+    (286460, uint256("0xb74bd460b2bdb5f0c873c13266cc277433629f1e7714f35f765b169a1e990736"));
+
 
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1538110800, // * UNIX timestamp of last checkpoint block
-    0,     // * total number of transactions between genesis and last checkpoint
+    1533767118, // * UNIX timestamp of last checkpoint block
+    600513,     // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
-    10000        // * estimated number of transactions per day after checkpoint
+    2000        // * estimated number of transactions per day after checkpoint
 };
 
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
@@ -93,11 +96,11 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x8C;
-        pchMessageStart[1] = 0x42;
-        pchMessageStart[2] = 0xC1;
-        pchMessageStart[3] = 0xFA;
-        vAlertPubKey = ParseHex("040adaedd077c8660ea6d2bcf5ef02372774ca553e29256c6ae963c7c368df79287148ffdc67ba6ba66b1b0f547c1dd3f008366ee8c1cb0d912ff63cdebce62fab");
+        pchMessageStart[0] = 0x54;
+        pchMessageStart[1] = 0xdc;
+        pchMessageStart[2] = 0x12;
+        pchMessageStart[3] = 0xae;
+        vAlertPubKey = ParseHex("04d2df519f53e2eaa4a7d7ff3347a360520c2f4b8f07d0241b5b6ba5ce8e3d6ecba5443696473a387adff27aa6bb72b952ff23026e088cff9f47cbb387ed52c326");
         nDefaultPort = 9333;
         bnProofOfWorkLimit = ~uint256(0) >> 1;
         nSubsidyHalvingInterval = 1050000;
@@ -106,81 +109,68 @@ public:
         nRejectBlockOutdatedMajority = 950;
         nToCheckBlockUpgradeMajority = 1000;
         nMinerThreads = 0;
-        nTargetTimespan = 1 * 60; // Rekel: 1 day
-        nTargetSpacing = 2 * 60;  // Rekel: 2 minutes
+        nTargetTimespan = 1 * 60; // Bitcoin Green: 1 day
+        nTargetSpacing = 2 * 60;  // Bitcoin Green: 2 minutes
         nMaturity = 10;
         nMasternodeCountDrift = 20;
         nMaxMoneyOut = 21000000 * COIN;
 
         /** Height or Time Based Activations **/
         nLastPOWBlock = 200;
-        nModifierUpdateBlock = 1; // we use the version 2 for RKL
+        nModifierUpdateBlock = 1; // we use the version 2 for BITG
 
         /**
          * Build the genesis block. Note that the output of the genesis coinbase cannot
          * be spent as it did not originally exist in the database.
          *
-         * genesis.py -a quark-hash -z "Regierung legt drei Diesel-Optionen vor. Rekel is Born 25-09-2018 :-)" -t 1537880400 -v 0 -p 04157d28e0f0301a8bab403bbf3955c076b2ba87f9641d45bab81d2ea145563a0196846833f9e9a97c80a04d34471ac4e1425d218d0fb88cf5c9d7d8fb3da0b94e
+         * genesis.py -a quark-hash -z "Even With Energy Surplus, Canada Unable to Meet Electricity Demands of Bitcoin Miners" -t 1516926684 -v 0 -p 04e5a8143f86ad8ac63791fbbdb8e0b91a8da88c8c693a95f6c2c13c063ea790f7960b8025a9047a7bc671d5cfe707a2dd2e13b86182e1064a0eea7bf863636363
          * 04ffff001d01042642544320426c6f636b20353031353932202d20323031372d31322d32392031353a34333a3337
          * algorithm: quark-hash
-         * merkle hash: afc5557636b7b5c1ff84068a26d661891196c4ff8e6822ed2f4883b53fa1762b
-         * pszTimestamp: Regierung legt drei Diesel-Optionen vor. Rekel is Born 25-09-2018 :-)
-         * pubkey: 04157d28e0f0301a8bab403bbf3955c076b2ba87f9641d45bab81d2ea145563a0196846833f9e9a97c80a04d34471ac4e1425d218d0fb88cf5c9d7d8fb3da0b94e
-         * time: 1537880400
+         * merkle hash: 07cbcacfc822fba6bbeb05312258fa43b96a68fc310af8dfcec604591763f7cf
+         * pszTimestamp: Even With Energy Surplus, Canada Unable to Meet Electricity Demands of Bitcoin Miners
+         * pubkey: 04e5a8143f86ad8ac63791fbbdb8e0b91a8da88c8c693a95f6c2c13c063ea790f7960b8025a9047a7bc671d5cfe707a2dd2e13b86182e1064a0eea7bf863636363
+         * time: 1516926684
          * bits: 0x1e0ffff0
          * Searching for genesis hash..
          * 16525.0 hash/s, estimate: 72.2 hgenesis hash found!
-         * nonce: 21279841
-         * genesis_hash: 0000070e2551543dcb397cbb054439e4c7294f4322a67135941bcc48fa13c34f
+         * nonce: 21256609
+         * genesis_hash: 000008467c3a9c587533dea06ad9380cded3ed32f9742a6c0c1aebc21bf2bc9b
          */
-        const char* pszTimestamp = "Regierung legt drei Diesel-Optionen vor. Rekel is Born. It sucks my Brain. 25-09-2018 :-)";
+        const char* pszTimestamp = "Even With Energy Surplus, Canada Unable to Meet Electricity Demands of Bitcoin Miners";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 0 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("04157d28e0f0301a8bab403bbf3955c076b2ba87f9641d45bab81d2ea145563a0196846833f9e9a97c80a04d34471ac4e1425d218d0fb88cf5c9d7d8fb3da0b94e") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("04e5a8143f86ad8ac63791fbbdb8e0b91a8da88c8c693a95f6c2c13c063ea790f7960b8025a9047a7bc671d5cfe707a2dd2e13b86182e1064a0eea7bf863636363") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime = 1538110800;
+        genesis.nTime = 1516926684;
         genesis.nBits = 0x1e0ffff0;
-        genesis.nNonce = 22184939;
-
-/*
-        printf("genesis.nTime = %u \n", genesis.nTime);
-        printf("genesis.nNonce = %u \n", genesis.nNonce);
-        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str()); //first this, then comment this line out and uncomment the one under.
-        printf("genesis.hashMerkleRoot = %s \n", genesis.hashMerkleRoot.ToString().c_str()); //improvised. worked for me, to find merkle root
-        printf("min nBit:  %08x\n", bnProofOfWorkLimit.GetCompact());
-        printf("genesis.nBits = %u \n", genesis.nBits);
+        genesis.nNonce = 21256609;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x"));
-        assert(genesis.hashMerkleRoot == uint256("0x"));
-*/
-
-        hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x09229261a9715b25eb4e1f63f35825df173b1cf1f41d1cf3dcf6309fd196614b"));
-        assert(genesis.hashMerkleRoot == uint256("0xd62f0a2c7743f50ccbf8ff80c88aaad2e653d89d389bb73b5c6e5399d89ec005"));
-
+        assert(hashGenesisBlock == uint256("0x000008467c3a9c587533dea06ad9380cded3ed32f9742a6c0c1aebc21bf2bc9b"));
+        assert(genesis.hashMerkleRoot == uint256("0x07cbcacfc822fba6bbeb05312258fa43b96a68fc310af8dfcec604591763f7cf"));
 
         // DNS Seeding
-        vSeeds.push_back(CDNSSeedData("46.232.248.21", "46.232.248.21"));
-        vSeeds.push_back(CDNSSeedData("37.221.192.88", "37.221.192.88"));
+        vSeeds.push_back(CDNSSeedData("seed1.savebitcoin.io", "seed1.savebitcoin.io"));
+        vSeeds.push_back(CDNSSeedData("seed2.savebitcoin.io", "seed2.savebitcoin.io"));
+        vSeeds.push_back(CDNSSeedData("seed3.savebitcoin.io", "seed3.savebitcoin.io"));
 
-        // Rekel addresses start with 'R'
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 60);
-        // Rekel script addresses start with '5'
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 11);
-        // Rekel private keys start with 'P'
-        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 56);
-        // Rekel BIP32 pubkeys start with 'xpub' (Bitcoin defaults)
+        // Bitcoin Green addresses start with 'G'
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 38);
+        // Bitcoin Green script addresses start with '3'
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 6);
+        // Bitcoin Green private keys start with 'K'
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 46);
+        // Bitcoin Green BIP32 pubkeys start with 'xpub' (Bitcoin defaults)
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
-        // Rekel BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
+        // Bitcoin Green BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
-        // Rekel BIP44 coin type is '222' (0x800000de)
+        // Bitcoin Green BIP44 coin type is '222' (0x800000de)
         // BIP44 coin type is from https://github.com/satoshilabs/slips/blob/master/slip-0044.md
         base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0xde).convert_to_container<std::vector<unsigned char> >();
 
@@ -196,8 +186,8 @@ public:
         fHeadersFirstSyncingActive = false;
 
         nPoolMaxTransactions = 3;
-        strSporkKey = "040adaedd077c8660ea6d2bcf5ef02372774ca553e29256c6ae963c7c368df79287148ffdc67ba6ba66b1b0f547c1dd3f008366ee8c1cb0d912ff63cdebce62fab";
-        strMasternodePoolDummyAddress = "RPkFr5js6GftfRY1NQK9bpTQZZLXmBiy3T";
+        strSporkKey = "04026c33ff67ad40db68e7ba4f6d858a56550a7aba460857d1cf0f34af4e7d090b255928df8135ee5343df02b88801635ef054da6ef3071ce69c2c3134acad54e9";
+        strMasternodePoolDummyAddress = "GSJVWUkt6HtSCY2SaJ2akeyJUg8bg1hW3S";
         nStartMasternodePayments = genesis.nTime + 86400; // 24 hours after genesis creation
 
         nBudget_Fee_Confirmations = 6; // Number of confirmations for the finalization fee
@@ -230,8 +220,8 @@ public:
         nRejectBlockOutdatedMajority = 75;
         nToCheckBlockUpgradeMajority = 100;
         nMinerThreads = 0;
-        nTargetTimespan = 1 * 60; // Rekel: 1 day
-        nTargetSpacing = 2 * 60;  // Rekel: 1 minute
+        nTargetTimespan = 1 * 60; // Bitcoin Green: 1 day
+        nTargetSpacing = 2 * 60;  // Bitcoin Green: 1 minute
         nLastPOWBlock = 200;
         nMaturity = 15;
         nMasternodeCountDrift = 4;
@@ -240,25 +230,25 @@ public:
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
         genesis.nTime = 1534570200;
-        genesis.nNonce = 24176473;
+        genesis.nNonce = 21148656;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x0000063148828fa9f08667cad3abc8277c4e1018db828197758eb47ff859f512"));
+        assert(hashGenesisBlock == uint256("0x000000938f4f20c6ccb3fea36539ade5af73d0bb45c55af64c7f7f1bfa5f3381"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
 
-        // Testnet Rekel addresses start with 'r'
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 122);
-        // Testnet Rekel script addresses start with '7' or '8'
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 17);
-        // Testnet private keys start with 'p'
-        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 118);
-        // Testnet Rekel BIP32 pubkeys start with 'tpub' (Bitcoin defaults)
+        // Testnet Bitcoin Green addresses start with 'g'
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 98);
+        // Testnet Bitcoin Green script addresses start with '5' or '6'
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 12);
+        // Testnet private keys start with 'k'
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 108);
+        // Testnet Bitcoin Green BIP32 pubkeys start with 'tpub' (Bitcoin defaults)
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
-        // Testnet Rekel BIP32 prvkeys start with 'tprv' (Bitcoin defaults)
+        // Testnet Bitcoin Green BIP32 prvkeys start with 'tprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
-        // Testnet Rekel BIP44 coin type is '1' (All coin's testnet default)
+        // Testnet bitcoin green BIP44 coin type is '1' (All coin's testnet default)
         base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x01).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
@@ -304,25 +294,16 @@ public:
         nRejectBlockOutdatedMajority = 950;
         nToCheckBlockUpgradeMajority = 1000;
         nMinerThreads = 1;
-        nTargetTimespan = 24 * 60 * 60; // Rekel: 1 day
-        nTargetSpacing = 2 * 60;        // Rekel: 1 minutes
+        nTargetTimespan = 24 * 60 * 60; // Bitcoin Green: 1 day
+        nTargetSpacing = 2 * 60;        // Bitcoin Green: 1 minutes
         bnProofOfWorkLimit = ~uint256(0) >> 1;
-        genesis.nTime = 1538110800;
+        genesis.nTime = 1516926684;
         genesis.nBits = 0x207fffff;
-        genesis.nNonce = 20542304;
-
-/*
-        printf("genesis.nTime = %u \n", genesis.nTime);
-        printf("genesis.nNonce = %u \n", genesis.nNonce);
-        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str()); //first this, then comment this line out and uncomment the one under.
-        printf("genesis.hashMerkleRoot = %s \n", genesis.hashMerkleRoot.ToString().c_str()); //improvised. worked for me, to find merkle root
-        printf("min nBit:  %08x\n", bnProofOfWorkLimit.GetCompact());
-        printf("genesis.nBits = %u \n", genesis.nBits);
-*/
+        genesis.nNonce = 20542300;
 
         hashGenesisBlock = genesis.GetHash();
         nDefaultPort = 29333;
-        assert(hashGenesisBlock == uint256("0xf74fda37a08d10c853cddb1db44560309ef38b6b5cc5bdfdbd8ea9ab423b7037"));
+        assert(hashGenesisBlock == uint256("0x229874aa8a92df3347600978e226ba57bc994b9fa291ea50519afafca2d50ed3"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //! Regtest mode doesn't have any DNS seeds.
